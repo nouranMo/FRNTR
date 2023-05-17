cae729d0e1cf1897d778292c881f3eb972476112
George Aziz <123029529+GeorgeAyy@users.noreply.github.com>
2023-05-18 01:51:16 +0300

    did admin adding furniture items modification and organized the code
    added database modification when adding furniture items and fixed the admin client side validation

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ import express from 'express';
 import session from 'express-session';
 import connectDB from './config/db.js';
 import User from './models/user.js';
+import Furniture from './models/furniture.js';
+import multer from 'multer';
+import path from 'path';
+import image from 'express-image';
 import * as dotenv from 'dotenv';
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Set up multer storage configuration
+const storage = multer.diskStorage({
+  destination: 'public/images',
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Use the original filename
+  }
+});
+const upload = multer({ storage });
 
 // Set up port and database URI
 const port = process.env.PORT;
@@ -19,17 +32,20 @@ connectDB();
 const app = express();
 
 // Configure session middleware
-app.use(session({
+app.use(
+  session({
     secret: 'FYNGZ_KEY',
     resave: false, // Set to false to avoid the deprecation warning
     saveUninitialized: true // Set to true or false based on your needs
-  }));
+  })
+);
 
 // Set the view engine to EJS
 app.set('view engine', 'ejs');
 
 // Serve static files from the 'public' directory
 app.use(express.static('public'));
+app.use('/images', image('images'));
 
 // Parse JSON requests
 app.use(express.json());
@@ -38,9 +54,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Import routers
-import index_router from "./routes/index.js";
-import product_router from "./routes/Products.js";
-import user_router from "./routes/User.js";
+import index_router from './routes/index.js';
+import product_router from './routes/Products.js';
+import user_router from './routes/User.js';
 
 // Register routers
 app.use('/', index_router);
@@ -76,6 +92,44 @@ app.post('/SignUp', async (req, res) => {
 
     // Redirect to the home page
     res.redirect('/');
+  }
+});
+
+// Handle POST request to create a new furniture item
+app.post('/furniture', upload.array('photo', 5), async (req, res) => {
+  // Extract data from the request body
+  const { productname, category, color, price, quantity, comments, photo } = req.body;
+
+  let imagePaths = [];
+
+  // Check if files were uploaded
+  if (req.files) {
+    imagePaths = req.files.map((file) => file.path);
+  }
+
+  // Check if the product already exists
+  const existingProduct = await Furniture.findOne({ productName: productname });
+  if (existingProduct) {
+    return res.render('AddProduct', { failMessage: 'Product already exists' });
+  }
+
+  // Create a new furniture object
+  const newFurniture = new Furniture({
+    productName: productname,
+    category: category,
+    color: color,
+    price: parseFloat(price),
+    quantity: parseInt(quantity),
+    comments: comments,
+    photo: photo
+  });
+
+  // Save the new furniture item to the database
+  try {
+    await newFurniture.save();
+    return res.render('AddProduct', { successMessage: 'Successfully added an item' });
+  } catch (error) {
+    return res.render('AddProduct', { failMessage: 'Failed to add item' });
   }
 });
 
---
+++ models/furniture.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const furnitureSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    enum: ['armchair', 'beds', 'benches', 'beanbags', 'chaise lounges', 'living room set', 'kids furniture', 'pets furniture', 'space friendly'],
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  comments: {
+    type: String,
+    required: false
+  },
+  photo: {
+    type: String,
+    required: false
+  }
+});
+
+const Furniture = mongoose.model('Furniture', furnitureSchema);
+
+export default Furniture;
