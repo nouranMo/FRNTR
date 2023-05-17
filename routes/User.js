968b5769184f28e83a5cd0b3eaa7aacb61d21bfc
nnouran <96547311+nouranMo@users.noreply.github.com>
2023-05-18 01:06:01 +0400

    made an account page to view the profile and changed tiny things in nav

--- routes/User.js
+++ routes/User.js
@@ -25,7 +25,8 @@ router.get('/clientProduct', function(req, res, next) {
 
 /* GET Sign Up page. */
 router.get('/SignUp', function(req, res, next) {
-  res.render('SignUp',{ userr: req.session.userr });
+  // res.render('SignUp',{ userr: req.session.userr });
+  res.render('SignUp');
 });
 
 /* GET Wishlist page. */
@@ -62,6 +63,9 @@ router.get('/orders', function(req, res, next) {
 router.get('/offers', function(req, res, next) {
   res.render('offers');
 });
-
+/* GET account page. */
+router.get('/account',function(req,res,next){
+res.render('account');
+});
 // Export the router
 export default router;
