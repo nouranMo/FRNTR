55d0971cde41e4d1333c36ddff6306f2ecb82933
gitzuzu <Zeina2101569@miuegypt.edu.eg>
2023-05-18 23:51:41 +0300

    successfully took the data

--- controllers/adminviewprodController.js
+++ controllers/adminviewprodController.js
@@ -1,10 +1,12 @@
 import Furniture from "../models/furniture.js";
+console.log("Retrieved products from the database:");
 const productsController = {
     viewAllProducts: async (req, res) => {
       try {
+        console.log("Inside viewAllProducts");
         // Retrieve all products from the database
         const products = await Furniture.find();
-        
+        console.log("Retrieved products from the database:", products);
         // Render the "products" view and pass the products data
         res.render("products", { products });
       } catch (error) {
