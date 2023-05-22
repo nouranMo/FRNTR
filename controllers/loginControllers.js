4d1382ec7cfb86b3bd32437e43b54cc9f590dbd7
nnouran <96547311+nouranMo@users.noreply.github.com>
2023-05-22 13:33:50 +0400

    edited name variable form

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -1,68 +1,67 @@
 // loginController.js
 import bcrypt from "bcryptjs"
 import User from "../models/user.js";
-const loginController = {};
+const loginController = {}; 
 
-loginController.login =  (req, res) => {
-    console.log("Login request received");
-  
-    const { email, pass } = req.body;
-  
-   
-  
-    if (email === 'admin@gmail.com' && pass === 'admin!.!') {
-      // Render the dashboard
-      return res.render("dashboard");
-    }
-  
-    try {
-      // Find the user in the database by email
-     User.findOne({ email })
-  .then((result)=>{
-  
-    if (!result) {
-      // User not found, display error message
-      //return res.status(401).json({ error: "Invalid email " });
-    
-      return res.render("login",{errors:"Invalid email or password"});
-    }
+loginController.login = (req, res) => {
+  console.log("Login request received");
 
-  
-  
-     if (result.password!=pass) {
+  const { email, pass } = req.body;
 
 
-    // Comparing the entered password with the hashed one.
 
-     bcrypt.compare(pas,result.password).then((isPasValid)=>{
-
-           if (!isPasValid) {
-
-      // Password does not match, display error message
-      console.log("error");
-      // return res.status(401).json({ error: "Invalid  password" });
-     
-  
-      return res.render("login",{errors:"Invalid email or password"});
-    }
-
-  
-  req.session.user=result;
-
-  });
-
-  
-
-  
-    // Password matches, render the account page with user data
-    // return res.render("account", { userP:result});
-    return res.redirect('/auth/account')
-  });
-     
-    } catch (error) {
-      console.error("Error during login:", error);
-      return res.status(500).json({ error: "Internal server error" });
-    }
+  if (email === 'admin@gmail.com' && pass === 'admin!.!') {
+    // Render the dashboard
+    return res.render("dashboard");
   }
-  
-  export default loginController;
+
+  try {
+    // Find the user in the database by email
+    User.findOne({ email })
+      .then((result) => {
+
+        if (!result) {
+          // User not found, display error message
+          //return res.status(401).json({ error: "Invalid email " });
+
+          return res.render("login", { errors: "Invalid email or password" });
+        }
+
+
+
+        if (result.password != pass) {
+
+
+          // Comparing the entered password with the hashed one.
+
+          bcrypt.compare(pass, result.password).then((isPasValid) => {
+
+            if (!isPasValid) {
+
+              // Password does not match, display error message
+              console.log("error");
+              // return res.status(401).json({ error: "Invalid  password" });
+
+
+              return res.render("login", { errors: "Invalid email or password" });
+            }
+
+
+          });
+        }
+
+        req.session.user = result;
+
+        // Password matches, render the account page with user data
+        // return res.render("account", { userP:result});
+        return res.redirect('/auth/account')
+      });
+
+  } 
+  catch (error) {
+    console.error("Error during login:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export default loginController;
