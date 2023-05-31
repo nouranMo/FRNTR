3e34e135e1c2f2f6b8ebc7f3fd0f9b23bc93df06
nnouran <96547311+nouranMo@users.noreply.github.com>
2023-06-01 01:10:09 +0400

    PRODUCT DETAILS IS OFFICALY DYNAMIC

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import multer from "multer";
 import productsController from "../controllers/adminviewprodController.js";
 import userviewproduct from "../controllers/userproductController.js";
+import itemproductdetail from "../controllers/itempageController.js";
 var router = Router();
-router.get('/itempage', function(req, res, next) {
-  res.render('itempage');
-});
+router.get('/itempage/:id',itemproductdetail.detail);
+
 
 /* GET client Product page. */
 router.get('/clientproduct/:category', userviewproduct.userview);
