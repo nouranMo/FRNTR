73209e73f81aa59802af9cf2636c7516cff36ca1
gitzuzu <Zeina2101569@miuegypt.edu.eg>
2023-06-01 15:07:54 +0300

    controller filter

--- controllers/userproductController.js
+++ controllers/userproductController.js
@@ -1,24 +1,33 @@
 import Furniture from "../models/furniture.js";
-const userviewproduct ={
- 
-    userview: async(req,res)=>{
 
-    const {category}=req.params;
-    const product= await Furniture.find({ category });
+const userviewproduct = {
+  userview: async (req, res) => {
+    const { category } = req.params;
+    const products = await Furniture.find({ category });
 
-    if(product){
-       
-        product.forEach((products) => {
-            if (products.photo && products.photo.length > 0) {
-                console.log("hello");
-              products.photo = products.photo.map((photo)=>photo.replace(/\\/g, '/').replace('public/','/'));
-            }
-          });
-          
+    if (products) {
+      products.forEach((product) => {
+        if (product.photo && product.photo.length > 0) {
+          product.photo = product.photo.map((photo) =>
+            photo.replace(/\\/g, "/").replace("public/", "/")
+          );
+        }
+      });
     }
-    console.log(product);
 
-res.render("clientproduct",{product});
-    },
+    // Find the highest and lowest prices
+    const highestPrice = Math.max(...products.map((product) => product.price));
+    const lowestPrice = Math.min(...products.map((product) => product.price));
+
+    console.log("Highest Price:", highestPrice);
+    console.log("Lowest Price:", lowestPrice);
+
+    res.render("clientproduct", {
+      product: products,
+      highestPrice,
+      lowestPrice,
+    });
+  },
 };
+
 export default userviewproduct;
