7bb79e54400c9e5e015ffa2efc976fa55634b10d
gitfarrah <125498363+gitfarrah@users.noreply.github.com>
2023-06-02 18:46:18 +0300

    added add to cart , still needs some fixes

--- routes/User.js
+++ routes/User.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-
+import Furniture from "../models/furniture.js";
+import furnitureController from "../controllers/addfurnitureController.js";
+import Cart from "../models/cart.js";
 // Create a new router instance
 var router = Router();
 
@@ -11,6 +13,50 @@ router.get('/cart', function(req, res, next) {
 });
 
 
+//Create Route for add item into cart
+
+router.post('/add-to-cart', function(req, res) {
+  // const userId = req.body.user._id; // Assuming you have implemented user authentication
+  const productId = req.body.productId;
+  const productName = req.body.productName;
+  const productPrice = req.body.productPrice;
+  const productQuantatity = req.body.productQuantatity;
+  Cart.findOne({ userId: "64629f1742f3bb2f1961437d" }, function(err, cart) {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error finding cart');
+    } else if (cart) {
+      // Cart exists, check if it already contains the product
+      const index = cart.items.findIndex(item => item.productId.equals(productId));
+      if (index !== -1) {
+        // Product already in cart, increment quantity
+        cart.items[index].quantity++;
+      } else {
+        // Product not in cart, add new item
+        cart.items.push({ productId: productId, productName: productName, productPrice: productPrice, quantity: 1 });
+      }
+      cart.save(function(err) {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error saving cart');
+        } else {
+          res.send('Item added to cart');
+        }
+      });
+    } else {
+      // Cart does not exist, create new cart
+      const newCart = new Cart({ userId: userId, items: { productId: productId, productName: productName, productPrice: productPrice, quantity: 1 } });
+      newCart.save(function(err) {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error saving cart');
+        } else {
+          res.send('Item added to cart');
+        }
+      });
+    }
+  });
+});
 
 
 
