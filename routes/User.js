08d4d43ad59ad4204652738051f5e787e5fd5299
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-06-05 05:04:29 +0300

    Added the controller but still need updates.

--- routes/User.js
+++ routes/User.js
@@ -2,10 +2,11 @@ import express  from 'express';
 import Furniture from "../models/furniture.js";
 import furnitureController from "../controllers/addfurnitureController.js";
 import Cart from "../models/cart.js";
+import editprofileController1 from "../controllers/editprofileController.js"
 // Create a new router instance
 var router = express.Router();
 import bodyParser from "body-parser";
-import cartController from "../controllers/cartController.js";
+import cartController from '../controllers/cartController.js';
 
 router.use(bodyParser.json());
 
@@ -17,6 +18,8 @@ router.get('/cart', function(req, res, next) {
 
 //Create Route for add item into cart
 router.post('/add-to-cart', cartController.addToCart ); 
+router.post('/editprofile', editprofileController1.editprofile);
+
 //   // const userId = req.body.user._id; // Assuming you have implemented user authentication
 //   console.log('req: '+req.body.productId)
 //   console.log('req: '+req.body.productName)
@@ -79,16 +82,18 @@ router.post('/add-to-cart', cartController.addToCart );
 
 
 
+
 /* GET Wishlist page. */
 router.get('/wishlist', function(req, res, next) {
   res.render('wishlist',{user:req.session.user===undefined?"":req.session.user});
 });
 
 
-
 router.get('/editprofile', function(req, res, next) {
   res.render('editprofile',{user:req.session.user===undefined?"":req.session.user});
 });
+
+
 
 
 
