8893eaffc146f8476a212c837860158bc8aad8c7
Yassmin Ezzat <yassmin2104022@miuegypt.edu.eg>
2023-06-07 05:39:34 +0300

    Refactored edit profile associated files.

--- controllers/editprofileController.js
+++ controllers/editprofileController.js
@@ -4,13 +4,12 @@ const editprofileController = {};
 editprofileController.editprofile = async (req, res) => {
   console.log("Edit request received");
 
-  const { Firstname, last, email, address, address2 } = req.body;
+  const { Firstname, last, address, address2 } = req.body;
   const id = req.body.id;
   let errors = {};
 
   if (Firstname.trim() === "") {
     errors.Firstname = "You must enter your first name!";
-    console.log(errors.Firstname);
   }
 
   if (last.trim() === "") {
@@ -41,9 +40,6 @@ editprofileController.editprofile = async (req, res) => {
       // User not found
       return res.status(404).send("User not found");
     }
-
-    console.log(updatedUser.firstName);
-
     // Redirect to the account page with the updated user
     return res.render("account", { user: updatedUser });
   } catch (error) {
